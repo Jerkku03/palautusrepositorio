5367662515c78255c683412dd60831bb9e578b9d
Jerkku03 <jere.hietikko@live.fi>
2024-05-13 14:09:20 +0300

    t. 2.9 valmis

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [nimiHaku, setNimiHaku] = useState('')
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+
 
   const addNote = (event) => {
     event.preventDefault()
@@ -20,27 +31,35 @@ const App = () => {
       return
     }
     const noteObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
-  
+    
     setPersons(persons.concat(noteObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+      filter shown with<input value={nimiHaku} onChange={(e) => setNimiHaku(e.target.value)} />
+      </div>
       <form>
+        <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNoteChange}/>
+          <br />
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit" onClick={addNote}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(nimi => 
-        <p key={nimi.name}>{nimi.name}</p>
+      {persons.filter(nimi => nimi.name.toLowerCase().includes(nimiHaku)).map(nimi => 
+        <p key={nimi.name}>{nimi.name} {nimi.number}</p>
       )}
     </div>
   )

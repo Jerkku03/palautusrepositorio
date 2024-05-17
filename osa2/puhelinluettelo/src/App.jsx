756f3e3346d6ea5c1b2b9c4d8d7c63e7215b2839
Jerkku03 <jere.hietikko@live.fi>
2024-05-17 11:04:01 +0300

    t. 2.14 valmis

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import noteService from './services/notes'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,14 +12,12 @@ const App = () => {
   const [nimiHaku, setNimiHaku] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    noteService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
   }, [])
-
-  console.log(persons)
 
   const handleNoteChange = (event) => {
     setNewName(event.target.value)
@@ -27,8 +26,6 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-
-
 
   const addNote = (event) => {
     event.preventDefault()
@@ -41,9 +38,31 @@ const App = () => {
       number: newNumber
     }
     
-    setPersons(persons.concat(noteObject))
+    noteService
+      .create(noteObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+      })
     setNewName('')
     setNewNumber('')
+  }
+
+  const delNum = (id) => {
+    const poistettava = persons.filter(person => person.id === id)
+
+    if (window.confirm(`haluatko poistaa henkilön ${poistettava[0].name}`)){
+      noteService
+        .poista(id).then(returnedNote => {
+          console.log(returnedNote)
+          setPersons(persons.filter(n => n.id !== returnedNote.data.id))
+        })
+        .catch(error => {
+          alert(
+            `the person '${poistettava.name}' was already deleted from server`
+          )
+          setPersons(persons.filter(n => n.id !== id))
+        })
+      }
   }
 
   return (
@@ -52,7 +71,7 @@ const App = () => {
       <Filter value={nimiHaku} onChange={(e) => setNimiHaku(e.target.value)} />
       <PersonForm newName={newName} handleNoteChange={handleNoteChange} newNumber={newNumber} handleNumberChange={handleNumberChange} addNote={addNote}/>
       <h2>Numbers</h2>
-      <Persons persons={persons} nimiHaku={nimiHaku}/>
+      <Persons persons={persons} nimiHaku={nimiHaku} del={delNum}/>
     </div>
   )
 
--- osa2/puhelinluettelo/src/components/Persons.jsx
+++ osa2/puhelinluettelo/src/components/Persons.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Persons = ({persons, nimiHaku}) => {
+const Persons = ({persons, nimiHaku, del}) => {
     return (
         <div>
             {persons.filter(nimi => nimi.name.toLowerCase().includes(nimiHaku)).map(nimi => 
-            <p key={nimi.name}>{nimi.name} {nimi.number}</p>
+            <p key={nimi.name}>{nimi.name} {nimi.number} <button onClick={(()=> del(nimi.id))}>delete</button></p>
             )}
         </div>
       )

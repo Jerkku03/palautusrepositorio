40d28b456a8daf3bb88ce50fbd7095fd9154aa17
Jerkku03 <jere.hietikko@live.fi>
2024-05-17 13:03:43 +0300

    t. 2.17 valmis

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -5,6 +5,7 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import noteService from './services/notes'
 import Notification from './components/Notification'
+import ErrorNotification from './components/ErrorNotification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -12,6 +13,7 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [nimiHaku, setNimiHaku] = useState('')
   const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -42,14 +44,19 @@ const App = () => {
         const vanha = persons.find(n => n.name === newName)
         noteService
           .update(vanha.id, noteObject).then(returnedNote => {
-            setPersons(persons.map(person => person.id !== vanha.id ? person : returnedNote.data))})
+            setPersons(persons.map(person => person.id !== vanha.id ? person : returnedNote.data))
             setSuccessMessage(
               `${vanha.name} number was changed`
             )
             setTimeout(() => {
               setSuccessMessage(null)
-            }, 5000)
-      } return}
+            }, 5000)})
+          .catch(error => {
+            setErrorMessage('information already removed')
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)})
+        }return}
 
     noteService
       .create(noteObject)
@@ -93,6 +100,7 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Notification message={successMessage}/>
+      <ErrorNotification message={errorMessage}/>
       <Filter value={nimiHaku} onChange={(e) => setNimiHaku(e.target.value)} />
       <PersonForm newName={newName} handleNoteChange={handleNoteChange} newNumber={newNumber} handleNumberChange={handleNumberChange} addNote={addNote}/>
       <h2>Numbers</h2>

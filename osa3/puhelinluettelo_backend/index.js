f9acc21a4f746f320891d3486afb6dd529c7ff24
Jerkku03 <jere.hietikko@live.fi>
2024-06-26 12:10:08 +0300

    osa 3.1 valmis

---
+++ osa3/puhelinluettelo_backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+    {
+      id: "1",
+      name: "Arto hellas",
+      number: '040-123456'
+    },
+    {
+      id: "2",
+      name: "Ada Lovelace",
+      number: '39-44-5323523'
+    },
+    {
+      id: "3",
+      name: "Dan Abramov",
+      number: '12-43-2345'
+    },
+    {
+        id: "4",
+        name: "Mary Poppendick",
+        number: '39-23-6456'
+      }
+  ]
+
+app.get('/api/persons', (request, response) => {
+  response.send(notes)
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

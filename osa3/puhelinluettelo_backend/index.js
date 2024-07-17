c8f6832bb93c8ddf8818972c1fec14fa9f0a3473
Jerkku03 <jere.hietikko@live.fi>
2024-07-17 13:07:08 +0300

    t. 3.13 ja 3.14 valmiit

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 var morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+require('dotenv').config();
+const Person = require('./models/person')
 
 
 app.use(express.static('dist'))
@@ -10,31 +12,10 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 morgan.token('body', (request) => JSON.stringify(request.body))
 
-let notes = [
-    {
-      id: "1",
-      name: "Arto hellas",
-      number: '040-123456'
-    },
-    {
-      id: "2",
-      name: "Ada Lovelace",
-      number: '39-44-5323523'
-    },
-    {
-      id: "3",
-      name: "Dan Abramov",
-      number: '12-43-2345'
-    },
-    {
-        id: "4",
-        name: "Mary Poppendick",
-        number: '39-23-6456'
-      }
-  ]
-
 app.get('/api/persons', (request, response) => {
-  response.send(notes)
+  Person.find({}).then(persons => {
+      response.json(persons.map(person => person.toJSON()))
+      })
 })
 
 app.get('/info', (request, response) => {
@@ -74,28 +55,24 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  const personName = body.name
+  const personNumber = body.number
+
   if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  for (i=0; i < notes.length; i++) {
-    if (notes[i].name == body.name)
-      return response.status(400).json(
-    { error: 'name must be unique' })
-  }
+  const person = new Person ({
+    name: personName,
+    number: personNumber
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
   
-
-  const note = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
 })
 
 const PORT = process.env.PORT || 3001

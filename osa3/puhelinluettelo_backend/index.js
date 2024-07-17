d0191c4b96b187866088df02d9616f7f373cc0ac
Jerkku03 <jere.hietikko@live.fi>
2024-07-17 17:06:57 +0300

    t. 3.15, 3.16, 3.17 ja 3.18 valmiit

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -19,7 +19,9 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    peopleAmount = notes.length
+    Person.find({}).then(persons => {
+      peopleAmount = persons.length
+    })
     const date = new Date()
     tied = `Phonebook has info for ${peopleAmount} people`
     aika = `${date.toDateString()}`
@@ -27,21 +29,24 @@ app.get('/info', (request, response) => {
   response.send(`<p> ${tied} <p> <p> ${aika} ${date.toTimeString()}<p>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
+            response.json(person.toJSON())
+          } else {
+            response.status(404).end()
+          }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -74,6 +79,40 @@ app.post('/api/persons', (request, response) => {
   })
   
 })
+
+app.put('/api/persons/:id', (request, response, next) =>{
+  const body = request.body
+
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

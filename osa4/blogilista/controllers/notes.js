41455850e1f7bf10314a114031cca6279e50ac70
Jerkku03 <jere.hietikko@live.fi>
2024-08-10 11:17:17 +0300

    t.4.1 valmis

---
+++ osa4/blogilista/controllers/notes.js
@@ -0,0 +1,61 @@
+const notesRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+notesRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user',{ username: 1, name: 1, id: 1 })
+	  response.json(blogs)
+})
+
+notesRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+
+  blog.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = notesRouter

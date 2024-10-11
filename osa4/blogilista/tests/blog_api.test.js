d5c1af1659cb5d0685fe1185f02ffe688f24ce56
Jerkku03 <jere.hietikko@live.fi>
2024-10-11 10:20:29 +0300

    t.4.8 valmis

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,40 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('../tests/test_helper')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+
+  blogObject = new Blog(helper.initialBlogs[2])
+  await blogObject.save()
+})
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/blog')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('right amount of blogs', async () => {
+  const response = await api.get('/api/blog')
+
+  assert.deepStrictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})

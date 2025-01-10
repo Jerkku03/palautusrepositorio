695917c04a50742905463a004c96a1fa957a8153
Jere Hietikko <jerehietikko@Jere-MacBook-Pro-2.local>
2025-01-10 12:02:27 +0200

    t.5.9 valmis

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,13 +1,21 @@
 import Togglable from "./Togglable"
 import blogService from "../services/blogs"
+import { useState } from "react"
 
 const Blog = ({ blog }) => {
+  const [likes, setLikes] = useState(blog.likes)
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
     border: 'solid',
     borderWidth: 1,
     marginBottom: 5
+  }
+
+  const Like = () => {
+    setLikes(likes + 1)
+    blogService.update(blog.id, {id: blog.id, title: blog.title, author: blog.author, url: blog.url, likes: blog.likes += 1})
   }
   
   return (
@@ -16,7 +24,7 @@ const Blog = ({ blog }) => {
     <Togglable buttonLabel="view">
       <br />
       {blog.url} <br />
-      likes {blog.likes} <button onClick={() => blogService.update(blog.id, {id: blog.id, title: blog.title, author: blog.author, url: blog.url, likes: blog.likes += 1})}>like</button><br />
+      likes {likes} <button onClick={() => Like()}>like</button><br />
       {blog.author} 
       </Togglable>  
   </div>

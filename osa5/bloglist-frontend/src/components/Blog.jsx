67340f243b78aa6296c3045f4dd70f3d634d2f72
Jere Hietikko <jerehietikko@Jere-MacBook-Pro-2.local>
2025-01-10 11:43:10 +0200

    t.5.8 valmis

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,15 +1,26 @@
 import Togglable from "./Togglable"
+import blogService from "../services/blogs"
 
-const Blog = ({ blog }) => (
-  <div style={{display: "flex"}}>
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  return (
+  <div style={blogStyle}>
     {blog.title}
     <Togglable buttonLabel="view">
       <br />
       {blog.url} <br />
-      likes {blog.likes} <button>like</button><br />
+      likes {blog.likes} <button onClick={() => blogService.update(blog.id, {id: blog.id, title: blog.title, author: blog.author, url: blog.url, likes: blog.likes += 1})}>like</button><br />
       {blog.author} 
       </Togglable>  
   </div>
-)
+  )
+}
 
 export default Blog

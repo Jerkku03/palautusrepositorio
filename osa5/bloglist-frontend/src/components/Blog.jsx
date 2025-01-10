eb4a35441521d94f8e9c7ab793dd515591cd8324
Jere Hietikko <jerehietikko@Jere-MacBook-Pro-2.local>
2025-01-10 10:57:53 +0200

    t.5.7 valmis

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,15 @@
+import Togglable from "./Togglable"
+
 const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
+  <div style={{display: "flex"}}>
+    {blog.title}
+    <Togglable buttonLabel="view">
+      <br />
+      {blog.url} <br />
+      likes {blog.likes} <button>like</button><br />
+      {blog.author} 
+      </Togglable>  
+  </div>
 )
 
 export default Blog

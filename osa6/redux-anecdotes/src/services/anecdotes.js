1141670621e7f124e7b608f705ccfe719293119c
Jere Hietikko <jerehietikko@Jere-MacBook-Pro-2.local>
2025-02-22 11:18:24 +0200

    t.6.15 valmis

--- osa6/redux-anecdotes/src/services/anecdotes.js
+++ osa6/redux-anecdotes/src/services/anecdotes.js
@@ -7,4 +7,10 @@ const getAll = async () => {
   return response.data
 }
 
-export default { getAll }
+const createNew = async (content) => {
+    const object = { content, id: (100000 * Math.random()).toFixed(0), votes: 0 }
+    const response = await axios.post(baseUrl, object)
+    return response.data
+  }
+
+export default { getAll, createNew }
